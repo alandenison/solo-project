b14e788a53f3986e1b36f4d126066bc98e11d22c
Alan Denison <alan79td@gmail.com>
2016-10-05 00:19:31 -0700

    add unit test for customer class

---
+++ CustomerService/CustomerServiceTests/CustomerTest.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using CustomerService.Models;
+
+namespace CustomerService.Tests
+{
+    public class CustomerTest
+    {
+        [Fact]
+        public void GetNameTest()
+        {
+            //Arrange
+            var customer = new Customer();
+            customer.Name = "Giggy Sanjan";
+
+            //Act
+            var result = customer.Name;
+
+            //Assert
+            Assert.Equal("Giggy Sanjan", result);
+        }
+    }
+}
